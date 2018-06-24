1f697c3e0cc98830ec535e245acbf110965bbbc4
Tijmen Bok <mikauschekzen@gmail.com>
2018-06-24 21:02:57 +0200

    add replay and world map buttons to post game screen

--- src/app/scenes/scene-postgame.js
+++ src/app/scenes/scene-postgame.js
@@ -2,11 +2,11 @@ function Scene_PostGame() {
   this.init.apply(this, arguments);
 }
 
-Scene_PostGame.prototype = Object.create(Scene_Base.prototype);
+Scene_PostGame.prototype = Object.create(Scene_MenuBase.prototype);
 Scene_PostGame.prototype.constructor = Scene_PostGame;
 
 Scene_PostGame.prototype.init = function() {
-  Scene_Base.prototype.init.call(this);
+  Scene_MenuBase.prototype.init.call(this);
   this.needed = $gameMap.needed;
   this.saved = $gameMap.saved;
   this.totalLemmings = $gameMap.totalLemmings;
@@ -18,6 +18,7 @@ Scene_PostGame.prototype.init = function() {
 }
 
 Scene_PostGame.prototype.create = function() {
+  Scene_MenuBase.prototype.create.call(this);
   // Stop Bgm
   AudioManager.stopBgm();
   // Add background
@@ -32,10 +33,12 @@ Scene_PostGame.prototype.create = function() {
   this.text.status.position.set(Core.resolution.x / 2, 40);
   this.text.status.anchor.set(0.5, 0);
   this.stage.addChild(this.text.status);
+  // Add replay button
+  this.addReplayButton();
+  // Add world map button
+  this.addWorldMapButton();
   // Fade in
   this.fadeIn();
-  // Add interactivity
-  Input.mouse.button.LEFT.onPress.add(this.continueGame, this);
 }
 
 Scene_PostGame.prototype.getResultText = function() {
@@ -47,21 +50,65 @@ Scene_PostGame.prototype.getResultText = function() {
 
 Scene_PostGame.prototype.continueGame = function() {
   if(this.success) {
-    this.fadeOut(function() {
-      while(!(SceneManager.current() instanceof Scene_MainMenu) && !(SceneManager.current() instanceof Scene_WorldMap)) {
-        SceneManager.pop();
-      }
-    }.bind(this));
+    this.doReturnToWorldMap();
   }
   else {
-    this.fadeOut(function() {
-      while(!(SceneManager.current() instanceof Scene_PreGame) && !(SceneManager.current() instanceof Scene_MainMenu) && !(SceneManager.current() instanceof Scene_WorldMap)) {
-        SceneManager.pop();
-      }
-    }.bind(this));
+    this.doRetry();
   }
 }
 
+Scene_PostGame.prototype.doReturnToWorldMap = function() {
+  this.fadeOut(function() {
+    while(!(SceneManager.current() instanceof Scene_MainMenu) && !(SceneManager.current() instanceof Scene_WorldMap)) {
+      SceneManager.pop();
+    }
+  }.bind(this));
+};
+
+Scene_PostGame.prototype.doRetry = function() {
+  this.fadeOut(function() {
+    while(!(SceneManager.current() instanceof Scene_PreGame) && !(SceneManager.current() instanceof Scene_MainMenu) && !(SceneManager.current() instanceof Scene_WorldMap)) {
+      SceneManager.pop();
+    }
+  }.bind(this));
+};
+
 Scene_PostGame.prototype.end = function() {
+  Scene_MenuBase.prototype.end.call(this);
   Input.mouse.button.LEFT.onPress.remove(this.continueGame, this);
 }
+
+Scene_PostGame.prototype.addReplayButton = function() {
+  // Create element
+  var elem = new UI_MenuButton(new Point(0, 0), "Replay");
+  let x = Math.floor(Core.resolution.x * 0.25 - elem.sprite.width / 2);
+  let y = Math.floor(Core.resolution.y * 0.75 - elem.sprite.height / 2);
+  elem.x = x;
+  elem.y = y;
+  // Add event
+  console.log(elem);
+  elem.onClick.add(function() {
+    this.doRetry();
+  }.bind(this));
+  // Add element
+  this.addUI(elem);
+  // Return element
+  return elem;
+};
+
+Scene_PostGame.prototype.addWorldMapButton = function() {
+  // Create element
+  var elem = new UI_MenuButton(new Point(0, 0), "World Map");
+  let x = Math.floor(Core.resolution.x * 0.75 - elem.sprite.width / 2);
+  let y = Math.floor(Core.resolution.y * 0.75 - elem.sprite.height / 2);
+  elem.x = x;
+  elem.y = y;
+  // Add event
+  elem.onClick.add(function() {
+    this.doReturnToWorldMap();
+  }.bind(this));
+  // Add element
+  this.addUI(elem);
+  // Return element
+  return elem;
+};
